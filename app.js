11e00083c7bdd203a89ec007f3ddfe3a6f4a1487
TheDigitalCrazyPainter <gilles@triptyk.eu>
2015-12-23 10:37:38 +0100

    exercice01

---
+++ app.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// let f= function(h,w){
+//   return h*w;
+// };
+// let d= function(h,w){
+//   return h/w;
+// }
+//
+// function calcul(fn) {
+//   return fn(4,5);
+// }
+// console.log(calcul(d));
+
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+// console.log("-------------")
+// for (let i =0; i < 8; i++) {
+//   console.log(i);
+// }
+{
+  let a =23;
+  console.log(a)
+}
