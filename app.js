92f3d3767af3715d86ff408a0e1c1d677b01532c
TheDigitalCrazyPainter <gilles@triptyk.eu>
2015-12-23 13:40:34 +0100

    exercice04

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
 "use strict";
+let http = require('http');
+let port = 3000;
 let fs = require('fs');
-let utils = require('./modules/tpk/utils');
-fs.readFile('./fake.txt','utf8', function (err, data) {
-  if (err) throw err;
-  console.log(utils.compteMesLignes(data));
-});
+let routes ={
+  "/api/friends":"datas/friends.json",
+  "/api/amis":"datas/friends.json"
+}
+let serv = http.createServer((req, res) =>
+  {
+
+    if (routes[req.url])
+    {
+      res.writeHead(200,
+      {
+        'Content-Type': 'text/json'
+      });
+      fs.readFile(routes[req.url], 'utf8', (err, data) =>
+      {
+        if (err) throw err;
+        res.end(data)
+      });
+
+    }
+    else
+    {
+      res.writeHead(404,
+      {
+        'Content-Type': 'text/html'
+      });
+      fs.readFile('public/404.html', 'utf8', (err, data) =>
+      {
+        if (err) throw err;
+        res.end(data);
+      });
+
+    }
+  })
+  .listen(port, () =>
+  {
+    console.log(`server listening at port ${port}`);
+  })
